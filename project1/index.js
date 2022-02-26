7a9821bd8735a02a40d198f28fdf2a9a91641891
Kirill Zubkov <kirya.zubkov.98@list.ru>
2022-02-26 23:43:19 +0300

    feat: final lesson

---
+++ project1/index.js
@@ -0,0 +1,105 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 600;
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let img = new Image();
+img.src = "./assets/shadow_dog.png";
+
+const spriteWidth = 575;
+const spriteHeight = 523;
+let gameFrame = 0;
+const staggerFrames = 5;
+let state = "fall";
+
+const changer = document.querySelector("#animations");
+changer.addEventListener("change", function (event) {
+  state = event.target.value;
+});
+
+let animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizz",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
+];
+let spriteAnimations = {};
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let i = 0; i < state.frames; i++) {
+    frames.loc.push({
+      x: i * spriteWidth,
+      y: index * spriteHeight,
+    });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+//This lesson explain how program animation, not for moving and etc, that is why i dont add properties like speed
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.height, canvas.width);
+
+  let position =
+    Math.floor(gameFrame / staggerFrames) % spriteAnimations[state].loc.length;
+
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[state].loc[position].y;
+  ctx.drawImage(
+    img,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
