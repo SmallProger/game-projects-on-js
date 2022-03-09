ed8e706e6a62ca51e2edb045200d14bf549e6bc8
Kirill Zubkov <kirya.zubkov.98@list.ru>
2022-03-09 21:07:30 +0300

    feat: center canvas and add sizes

--- project6/index.js
+++ project6/index.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById("movement");
 const ctx = canvas.getContext("2d");
+const canvasWidth = 500;
+const canvasHeight = 700;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
